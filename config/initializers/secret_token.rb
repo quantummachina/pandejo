a0062f309ee0f5f3ec04ce54a927e107e5f336c3
Jorge F. Contreras <windblows3@yahoo.com>
2013-08-31 01:20:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pandejo::Application.config.secret_token = '6603480a3e182ed4243f898238b06c146950fbddbcd2f150956100cd8614fd705a2650e3c44f851cf1235453360005537fbf40f7fc7c7686558c24cae81824ea'
